f0052fed9cd193f85844d4b1f537f69daeb5f464
daxang <hysje0113@gmail.com>
2024-07-08 10:08:13 +0900

    헤더 수정

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 $(function () {
     // Header, Footer 레이아웃
-    $("#header").load("/public/layout/header.html");
+    $("#header01").load("/public/layout/header01.html");
+    $("#header02").load("/public/layout/header02.html");
     $("#footer").load("/public/layout/footer.html");
 });
